288c76be6b924d22a5e5b5b25c785a08dd758cf6
Chetan Rane <chethran@in.ibm.com>
2019-07-31 00:42:27 +0530

    Added custom hooks

---
+++ src/hooks.tsx
@@ -0,0 +1,15 @@
+import { useEffect, useState } from 'react';
+
+export const useFetch = (url: string, def: any) => {
+    const [data, setData] = useState(def);
+    const [loading, setLoading] = useState(true);
+    useEffect(() => {
+      ( async () => {
+      const response = await fetch(url);
+      const json = await response.json();
+      setData(json);
+      setLoading(false);
+      })()
+    }, [url]);
+    return [data, loading];
+  }

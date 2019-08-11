cc496613357c2349b2865667dd924fd64559417f
Chetan D Rane <chetan_rane@persistent.com>
2019-08-11 21:29:42 +0530

    cleaned up the code

---
+++ src/_unittest.test.tsx
@@ -0,0 +1,21 @@
+import * as React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { shallow, configure } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import { DynamicTabs } from "./components/SimpleTabs";
+
+configure({ adapter: new Adapter() });
+
+describe("Testing App", () => {
+  it("renders without crashing", () => {
+    const div = document.createElement("div");
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it("Check for Dynamic Tabs", () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find(DynamicTabs).length).toBeGreaterThan(0);
+  });
+});
